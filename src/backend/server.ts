6ab8f9836876120c39f672d406652dddbfe3047a
Iarley Souza <iarleysouza615@gmail.com>
2025-02-12 18:14:20 -0300

    update

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -7,13 +7,12 @@ class App {
     private app: Application;
     private http: http.Server;
     private io: Server;
+    private usersOnline: { [key: string]: string } = {};
 
     constructor() {
         this.app = express();
         this.http = http.createServer(this.app);
         this.io = new Server(this.http);
-
-        // Aumentar o limite de tamanho para 10MB
         this.io = new Server(this.http, {
             maxHttpBufferSize: 10e6 // 10MB
         });
@@ -22,36 +21,48 @@ class App {
         this.setupRoutes();
     }
     listenServer() {    
-        const PORT = process.env.PORT || 3000; // Porta dinâmica ou padrão
+        const PORT = process.env.PORT || 3000; 
         this.http.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
     }
     listenSocket() {
         this.io.on('connection', (socket) => {
             console.log('user connected => ', socket.id);
 
+            // Quando um usuário entra no chat
+            socket.on('userJoined', (userName) => {
+                this.usersOnline[socket.id] = userName;
+                this.io.emit('updateUsers', Object.values(this.usersOnline));
+            });
+
+            // Quando um usuário envia uma mensagem
             socket.on('message', (msg) => {
                 console.log(`Received message from ${msg.name}: ${msg.content}`);
-                // Envie a mensagem de volta para o remetente
                 socket.emit('message', { ...msg, fromUser: true });
-
-                // Reenvie a mensagem para os outros clientes
                 socket.broadcast.emit('message', { ...msg, fromUser: false });
             });
 
+            // Quando um usuário está digitando
             socket.on('typing', (name) => {
-                socket.broadcast.emit('typing', name); // Inclui o nome de quem está digitando
+                socket.broadcast.emit('typing', name); 
             });
+
+            // Quando um usuário para de digitar
             socket.on('stopTyping', () => {
-                socket.broadcast.emit('stopTyping'); // Emitir que a pessoa parou de digitar
+                socket.broadcast.emit('stopTyping');
             });
+
+            // Quando um usuário envia uma imagem
             socket.on('image', (data) => {
                 console.log(`Imagem recebida de ${data.name}`);
-                
-                // Envia de volta para quem enviou
                 socket.emit('image', data);
-            
-                // Envia para todos os outros usuários
                 socket.broadcast.emit('image', data);
+            });
+        
+            // Quando um usuário desconecta
+            socket.on('disconnect', () => {
+                console.log('Usuário desconectado:', socket.id);
+                delete this.usersOnline[socket.id];
+                this.io.emit('updateUsers', Object.values(this.usersOnline));
             });
         });
         
@@ -63,7 +74,5 @@ class App {
         });
     }
 }
-
 const app = new App()
-
 app.listenServer()
